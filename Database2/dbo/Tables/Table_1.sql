a5a0ea78881f30f46db534e85dd6be6bce4985b8
newuser <newuser@EC2AMAZ-QL6OER3>
2022-05-04 12:08:05 +0000

    Add project files.

---
+++ Database2/dbo/Tables/Table_1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table_1] (
+    [id]        INT           NULL,
+    [FirstName] VARCHAR (MAX) NULL,
+    [Address]   VARCHAR (50)  NULL
+);
+
---
+++ Database2/dbo/Tables/table_2.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[table_2] (
+    [ID]        INT           NULL,
+    [Name]      VARCHAR (255) NULL,
+    [FirstName] VARCHAR (255) NULL,
+    [Address]   VARCHAR (255) NULL
+);
+
